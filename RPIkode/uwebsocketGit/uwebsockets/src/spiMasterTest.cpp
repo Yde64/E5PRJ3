96d738b12e485e990b708715e0d5a175303229f4
Rybner888 <55833311+Rybner888@users.noreply.github.com>
2020-05-24 11:52:24 +0200

    Webserver Test
    Test af Webserver til modultest.

---
+++ RPIkode/uwebsocketGit/uwebsockets/src/spiMasterTest.cpp
@@ -0,0 +1,73 @@
+#include "spiMaster.h"
+#include <math.h>
+
+spiMaster::spiMaster(int data)
+{
+  data_ = data;
+}
+
+void spiMaster::listen()
+{
+  int spi, gpio, log, sek, ms;
+  int buf[20];
+  char buf2[200];
+  char buf3[200];
+
+  
+
+  while(1)
+  {
+    
+    sleep(1);
+
+    buf3[0] = 1;
+
+    //gpio = open("/dev/mygpio22", O_RDWR);
+    //read(gpio, buf3, 2);
+
+    //cout << "mygpio[0]: " << buf3[0] << endl;
+
+    if (buf3[0] == 1)
+    {
+      //Starter SPI kommunikation
+      //spi = open("/dev/SPI_0", O_RDONLY);
+      //sleep(0.1);
+      //read(spi, buf, 3);
+      //close(spi);
+
+        sek = 101;
+        ms = 5;
+
+     //cout << "sek: " << sek << endl; //DEBUG
+     //cout << "ms: " << ms << endl; //DEBUG
+
+        //Tider skrives til log
+        log = open("/www/pages/log.txt", O_WRONLY | O_APPEND | O_CREAT);
+        sprintf(buf2, "%i: Chug tid: %i:%is\n", count_, sek, ms);
+        dprintf(log, buf2, strlen(buf2)); 
+        close(log);
+
+      //Tid counter tælles op
+        count_++;
+      
+      //Interrupt nulstilles
+      //write(gpio, "0", 2);
+
+        buf3[0] = 0;
+      
+    }
+  
+  sleep(1);
+
+  }
+}
+
+int spiMaster::getdata()
+{
+return 0;
+}
+
+void spiMaster::setcount(int count)
+{
+  count_ = count;
+}
